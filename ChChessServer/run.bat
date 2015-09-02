d6b1f3d3081e0ce0ce9942c17cccbbf7babc0a40
wdmir <mir3@163.com>
2015-09-02 14:39:41 +0800

    ss
    ss

--- ChChessServer/run.bat
+++
@@ -1,2 +0,0 @@
-java -jar C:\D\kuaipan\521266750_qq_com\JavaServer\ChChessServer\dist\ChChessServer.jar
-pause
--- RecordServer/run.bat
+++
@@ -1,3 +0,0 @@
-title ¼ÇÂ¼·şÎñ
-java -jar C:\D\kuaipan\521266750_qq_com\JavaServer\RecordServer\dist\RecordServer.jar
-pause
